a8e26bffe3fa6bcf7aa5a3a5c6c03a1846b6d33b
Tim Moloney <tmoloney@trihydro.com>
2020-10-29 16:14:23 -0600

    revised fluid level data processing code to write csv

--- FluidLevels.R
+++ FluidLevels.R
@@ -8,6 +8,7 @@ mdbConnect<-odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb)};DBQ=M:/I
 mdbConnect<-odbcConnectAccess("M:/ItoN/MidnightSunTrinityJV/Modeling/AFP6/Analytical Data/AFP06.mdb")
 load("scrns_select.Rda")
 GWD<-sqlFetch(mdbConnect, "GWD")
+lvls_df<-tibble(well=character(), start_date=date(), last_date=date(), last_meas=numeric(), last10mean=numeric())
 
 wells<-unique(df$LOCID)
 for (well in wells) {welldata<-GWD[GWD$LOCID==well,]
@@ -16,19 +17,19 @@ last10<-welldata
 if (nrow(last10)>10) {last10<-welldata[(nrow(welldata)-9):(nrow(welldata)),]
 }
 n<-as.numeric(last10$STATDEP)
-ndfreq<-sum(is.na(n))/length(n)
+last10mean<-mean(na.omit(n))
 last_meas<-last10[nrow(last10),8]
-last_date<-last10[nrow(last10),3]
-
-
-
-
-well_results<-data.frame(well, pers, ndfreq, median_thick,last_thick,last_date)
-names(well_results)<-names(pers_df)
-pers_df<-rbind(pers_df,well_results)
+last_date<-as.Date(last10[nrow(last10),3])
+start_date<-as.Date(welldata[1,3])
+well_results<-data_frame(well, start_date, last_date, last_meas, last10mean)
+names(well_results)<-names(lvls_df)
+lvls_df<-rbind(lvls_df,well_results)
 errorCondition(next(well))
 }
-pers_df<-as_tibble(pers_df)
-pers_df$last_thick<-as.numeric(pers_df$last_thick)
-pers_df$last_thick[is.na(pers_df$last_thick)]<-0
-save(pers_df,file="LNAPL_pers_thick.Rda")
+
+lvls_merge<-merge(df,lvls_df,by.x="LOCID",by.y='well')
+lvls_merge<-lvls_merge %>% mutate(water_elev=MPELEV-last10mean)
+lvls_merge<-lvls_merge[is.numeric(lvls_merge$water_elev),]
+
+write_csv(lvls_merge,"fluid_levels.csv")
+
