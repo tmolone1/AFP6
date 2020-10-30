73dd98f83b51937ab7ff1745ccd7c8c9c9173dd9
Tim Moloney <tmoloney@trihydro.com>
2020-10-30 08:47:15 -0600

    extracted lith data from table 3 master well list

--- Tbl3_lith.R
+++ Tbl3_lith.R
@@ -12,44 +12,40 @@ shp<-readOGR("wellscreens.shp")
 plot(shp)
 plot(shp[shp$LOCID %in% intersect(df$LOCID,unique(data$`Well Identification`)),])
 
-pvt<-data[data$`Well Identification` %in% df$LOCID,c(4,11:15,17,30)]
-long<-pivot_longer(pvt, 2:8, names_to = "Lithology")
+pvt<-data[data$`Well Identification` %in% df$LOCID,c(4,11:15)]
+long<-pivot_longer(pvt, 2:6, names_to = "Lithology")
 
 locs<-unique(long$`Well Identification`)
 liths<-unique(long$Lithology)
-i=1
 long$to<-NA
+out<-tibble(head(long))
+out[seq(1:nrow(out)),seq(1:ncol(out))]<-NA
 for (loc in locs) {dat<-long[long$`Well Identification`==loc,]
-  for (i in 1:5) {
-    if (i<5) {dat[dat$Lithology==liths[i],"to"]<-dat[dat$Lithology==liths[i+1],"value"]
-    i=i+1
+dat<-long[long$`Well Identification`==loc,]
+dat$value<-as.numeric(dat$value)
+dat$to<-as.numeric(dat$to) 
+dat<-dat %>% arrange(value) 
+liths<-dat$Lithology
+i=1
+    for (lith in liths) {n<-dat[(which(dat$Lithology==lith)+i),]$value
+      while (is.na(n)) {
+        if (which(dat$Lithology==lith)<nrow(dat)) {i<-i+1
+          n<-dat[(which(dat$Lithology==lith)+i),]$value
+        }
+        if (is.na(n)) {
+         n<-max(na.omit(dat$value))+1
+         dat[dat$Lithology==lith,]$to<-n
+        }
+        dat[dat$Lithology==lith,]$to<-n
+      }
+    dat[dat$Lithology==lith,]$to<-n
+    if (is.na(dat[(which(dat$Lithology==lith)),]$value)) {
+      dat[dat$Lithology==lith,]$to<-NA
     }
-    
-    
-    
-dat[dat$Lithology==liths[i],"to"]<-dat[dat$Lithology==liths[i],"value"]+1
-    i=1
-  }
-  }
+    }
+out<-rbind(out,dat)
 }
+out<-out[!is.na(out$value),]
 
 
-
-
-
-
-write_csv(long,"tbl3_lith.csv")
-if (nrow(last10)>10) {last10<-welldata[(nrow(welldata)-9):(nrow(welldata)),]
-}
-n<-as.numeric(last10$STATDEP)
-last10mean<-mean(na.omit(n))
-last_meas<-last10[nrow(last10),8]
-last_date<-as.Date(last10[nrow(last10),3])
-start_date<-as.Date(welldata[1,3])
-well_results<-data_frame(well, start_date, last_date, last_meas, last10mean)
-names(well_results)<-names(lvls_df)
-lvls_df<-rbind(lvls_df,well_results)
-errorCondition(next(well))
-}
-
-lvls_merge<-merge(df,lvls_df,by.x="LOCID",by.y='well')
+write_csv(out,"tbl3_lith.csv")
