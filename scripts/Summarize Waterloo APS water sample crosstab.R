fc298e27430c710e11019a41c2b36aff5b0eaeb1
tmolone1 <tpmoloney@gmail.com>
2021-03-25 14:06:38 -0600

    revised things

--- scripts/Summarize Waterloo APS water sample crosstab.R
+++ scripts/Summarize Waterloo APS water sample crosstab.R
@@ -5,7 +5,7 @@ library(stringr)
 library(janitor)
 options(scipen=0)
 #read data and tidy
-water_samples<-read_excel("./data/Table 3 APS Analytical Summary Table_Export.xlsx")
+water_samples<-read_excel("./data/GCAL export_2020.0624_flat.xlsx")
 water_samples<-clean_names(water_samples)
 split<-str_split(water_samples$sample_id,"-",simplify=TRUE)
 colnames(split)<-c("site","hole_id","depth_ft_bgs","unknown")
@@ -25,25 +25,21 @@ water_samples<-water_samples %>% filter(!is.na(depth_ft_bgs_num))
 
 #edit hole ids to match collar table
 water_samples$hole_id<-str_remove_all(water_samples$hole_id," ")
-water_samples$hole_id<-str_remove(water_samples$hole_id,"DPT")
-water_samples$hole_id<-str_pad(water_samples$hole_id,pad = '0',2,side ='left')
 water_samples$hole_id<-str_c(water_samples$hole_id,"A")
-water_samples$hole_id<-str_c("DPT",water_samples$hole_id)
 
 #process zs and Js in result column
-water_samples$result_num<-ifelse(str_detect(water_samples$result,"U"),0,water_samples$result)
-##water_samples$result<-str_replace(water_samples$result,"z","<")
+water_samples$result_num<-ifelse(str_detect(water_samples$result,"z"),0,water_samples$result)
+water_samples$result<-str_replace(water_samples$result,"z","<")
 water_samples$result_num<-str_remove(water_samples$result_num,"J")
 water_samples$result_num<-parse_number(water_samples$result_num)
-water_samples$result_label<-ifelse(str_detect(water_samples$result,"U"),"ND",water_samples$result_num)
 
 # remove ms/msds
-remove<-c("MS", "MSD")
+remove<-c("MS, MSD")
 water_samples<-water_samples %>% filter(!unknown %in% remove)  
 rm(remove)
 
 #summarize max concentrations by location for selects constituents of interest
-analys<-c("Benzene","Trichloroethene", "Vinyl chloride" ,"cis-1,2-Dichloroethene", "Chromium VI")
+analys<-c("Benzene","Trichloroethene", "Vinyl chloride" ,"cis-1,2-Dichloroethene", "Chromium VI (Dissolved)")
 tbl<-water_samples %>% filter(analyte %in% analys) %>% group_by(hole_id, analyte) %>% summarize(max.detect=max(result_num))
 tbl$depth<-NA
 tbl$units<-NA
@@ -55,4 +51,6 @@ for (i in 1:nrow(tbl)) {
 tbl$depth<-as.character(output_column(tbl$depth))
 tbl$units<-as.character(output_column(tbl$units))
 write_csv(tbl,"./outputs/summary_max_concentrations.csv")
+
+
 
--- scripts/Write_Leaprog_Inputs_forAnalytical.R
+++ scripts/Write_Leaprog_Inputs_forAnalytical.R
@@ -1,9 +1,11 @@
 source("./scripts/Summarize Waterloo APS water sample data.R")
-width<-0.5
-analy<-"Benzene"
+width<-1
+shifts<-c(0,0,1,-1,0)
+# analy<-"Vinyl chloride"
 for (analy in analys) {
+  shift<-shifts[which(analys==analy)]
 tbl<-water_samples %>% filter(analyte == analy, !(unknown %in% (c("MS","MSD")))) %>% 
-  mutate(from=depth_ft_bgs_num-width,to=depth_ft_bgs_num+width) %>% select(hole_id,collect_date,analyte,from,to,result_num,units,result,qualifier,rdl)
+  mutate(from=depth_ft_bgs_num-width+shift,to=depth_ft_bgs_num+width+shift) %>% select(hole_id,collect_date,analyte,from,to,result_num,units,result_label,result,qualifier,mdl,pql)
 
 write_csv(tbl,path=paste0("./outputs/",analy,".csv"))
 }
