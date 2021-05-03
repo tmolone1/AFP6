3551b9f98a152eeffb32350a51b0405766d5200a
tmolone1 <tpmoloney@gmail.com>
2021-05-03 10:42:12 -0600

    made some edits to the summary max concentrations for the APS water sample data.

--- scripts/Write_Leaprog_Inputs_forAnalytical.R
+++ scripts/Write_Leaprog_Inputs_forAnalytical.R
@@ -9,3 +9,11 @@ tbl<-water_samples %>% filter(analyte == analy, !(unknown %in% (c("MS","MSD"))))
 
 write_csv(tbl,path=paste0("./outputs/",analy,".csv"))
 }
+
+## write file for maxumum concentration at each location
+tbl$units<-rep(tbl$units[1],nrow(tbl))
+collar<-read_excel("./data/Collar_appends_.xlsx")
+collar$hole_id<-collar$LOCID
+tbl_join<-left_join(tbl,collar, by="hole_id")
+tbl_join<-tbl_join[,c(1:5,7:11)]
+write_csv(tbl_join,"./outputs/summary_max_concentrations.csv")
